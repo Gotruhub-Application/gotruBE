2f98f4716a0797eef98b51919392fb75432f611e
nwafor6 <nwaforglory6@gmail.com>
2024-07-28 16:54:11 +0100

    removed test

--- src/controllers/organization/monitorFeature/organization.monitor.controller.ts
+++ src/controllers/organization/monitorFeature/organization.monitor.controller.ts
@@ -559,14 +559,6 @@ export class AttendanceController {
                 };
 
                 if(user?.subUnit != schedule?.subUnit) return failedResponse(res, 400, "You cannot take attendance in another sub-unit."); 
-            }else if (role === "staff") {
-                // Ensure the user ID is in the coordinators array
-                const isCoordinator = schedule?.coordinators.some(
-                    coordinatorId => coordinatorId.toString() === userId.toString()
-                );
-                if (!isCoordinator) {
-                    return failedResponse(res, 400, "You are not this course's coordinator.");
-                }
             }
             // else if(role === "staff"){
             //     if(!schedule?.coordinators.includes(userId) ) return failedResponse(res, 400, "You are not this course's coordinator."); 
