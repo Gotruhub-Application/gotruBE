6283d3ce199a135a83d101b831c5175eb7e0df90
nwafor6 <nwaforglory6@gmail.com>
2024-12-27 13:00:49 +0100

    added staff notifications

--- src/controllers/organization/tradeFeatures/organization.trade.controllers.ts
+++ src/controllers/organization/tradeFeatures/organization.trade.controllers.ts
@@ -857,7 +857,7 @@ export class OrderController {
           
           // if not unitId or Subunit, throw error
           if (!unitId &&!subunitId) {
-            throw new Error("Please provide either unitId or subunitId");
+            return failedResponse(res, 400, `Please provide either unitId or subunitId`);
           }
           
           let userQuery: any = { };
